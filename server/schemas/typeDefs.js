95a47d2e2ced0b96e375edac5527c4f9b20a686b
RayceWheat <RayceWheat@gmail.com>
2022-01-05 02:26:45 -0600

    created typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+// Creating our typeDefs
+const typeDefs = gql`
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    
+    type Book {
+        _id: ID
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        author: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
