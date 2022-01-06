8e00a951dff31a567e588127d927ed94f0ae0d89
RayceWheat <RayceWheat@gmail.com>
2022-01-06 01:10:58 -0600

    refactored front end

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,10 +2,15 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,25 +31,22 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+          variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      Auth.login(data.login.token);
+
+      } catch (err) {
+        console.error(err);
+        setShowAlert(true);
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+      setUserFormData({
+        username: '',
+        email: '',
+        password: '',
+      });
   };
 
   return (
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [addUser, { error }] = useMutation(ADD_USER);
+
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -28,26 +33,22 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const { data } = await addUser({
+          variables: { ...userFormData },
+      });
+          console.log(data);
+          Auth.login(data.addUser.token);
+      } catch (err) {
+          console.error(err);
+          setShowAlert(true);
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+      setUserFormData({
+        username: '',
+        email: '',
+        password: '',
+      });
+    };
 
   return (
     <>
